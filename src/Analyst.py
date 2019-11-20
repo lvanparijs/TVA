1f2a99ad23f05ed45e8a81e3bcef34a600db3109
lvanp <lvanparijs@gmail.com>
2019-11-20 13:58:32 +0100

    Implemented Compromising voting

--- src/Analyst.py
+++ src/Analyst.py
@@ -2,25 +2,25 @@ import numpy as np
 import itertools
 
 
-m = 4 #Number of voting alternatives
-n = 5 #Number of voters
+m = 6 #Number of voting alternatives
+n = 6 #Number of voters
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 #Binary matrices representing the voting schemes (Examples given for a 4*5 matrix)
-plurality_voting = np.append(np.ones((1,n)),np.zeros((m-1,n))).reshape((m,n))
+plurality_voting = np.append(np.ones((1,n)),np.zeros((m-1,n))).reshape((m,n)).astype(int)
 #[[1 1 1 1 1]
 # [0 0 0 0 0]
 # [0 0 0 0 0]
 # [0 0 0 0 0]]
 
-voting_for_two = np.append(np.ones((2,n)),np.zeros((m-2,n))).reshape((m,n))
+voting_for_two = np.append(np.ones((2,n)),np.zeros((m-2,n))).reshape((m,n)).astype(int)
 #[[1 1 1 1 1]
 # [1 1 1 1 1]
 # [0 0 0 0 0]
 # [0 0 0 0 0]]
 
-anti_plurality_voting = np.append(np.ones((m-1,n)),np.zeros((1,n))).reshape((m,n))
+anti_plurality_voting = np.append(np.ones((m-1,n)),np.zeros((1,n))).reshape((m,n)).astype(int)
 #[[1 1 1 1 1]
 # [1 1 1 1 1]
 # [1 1 1 1 1]
@@ -75,14 +75,13 @@ def true_voting(pref_matrix, voting_scheme):
                 unique = np.arange(0,m,1)
                 unique = [chr(int(x) + 65) for x in unique]
                 counts = [int(x) for x in counts]
-                print(counts)
-                print(unique)
         else:
                 res_matrix = pref_matrix*voting_scheme
+
                 unique, cnts = np.unique(res_matrix, return_counts=True)
                 unique = [chr(int(x)+64) for x in unique]
 
-                for i in range(0, len(unique)):
+                for i in range(1, len(unique)):
                         counts[chars.index(unique[i])] = cnts[i]
 
         #Zipping it and putting it in a dictionary for nice viewing purposes :)
@@ -106,28 +105,79 @@ def true_voting(pref_matrix, voting_scheme):
         return ranked_outcome, overall_happiness
 
 def strategic_voting(pref_matrix, voting_scheme, ranked_outcome):
+        out = []
         #For each voter
         for i in range(0,n):
-                happy1 = happiness_level(i, pref_matrix, ranked_outcome)
+                #Bullet
+                happy_orig = happiness_level(i, pref_matrix, ranked_outcome)
 
                 print("==========================")
                 print("VOTER " + str(i))
-                print("True Happiness: " + str(happy1))
+                print("True Happiness: " + str(happy_orig))
                 print("True voting: " + str(pref_matrix[:, i]))
                 print("True Outcome: "+str(ranked_outcome))
 
-                pref = pref_matrix[:,i].copy()
-                pref = bullet_vote(pref)
-                new_pref_matrix = pref_matrix.copy()
-                new_pref_matrix[:,i] = pref
-                outcome, _ = true_voting(new_pref_matrix,voting_scheme)
-                happy2 = happiness_level(i, new_pref_matrix, outcome)
+                pref_bull = pref_matrix[:,i].copy()
+                compromising_vote(pref_bull)
+                pref_bull = bullet_vote(pref_bull)
 
-                print("Strategic Happiness: "+str(happy2))
-                print("Bullet Voting: "+str(pref))
-                print("Bullet Outcome: "+str(outcome))
-                print(happy2>happy1)
+                bull_pref_matrix = pref_matrix.copy()
+                bull_pref_matrix[:,i] = pref_bull
+                outcome_bull, _ = true_voting(bull_pref_matrix,voting_scheme)
+                happy_bull = happiness_level(i, bull_pref_matrix, outcome_bull)
                 print("==========================")
+                print("BULLET")
+                print("Bullet Happiness: "+str(happy_bull))
+                print("Bullet Voting: "+str(pref_bull))
+                print("Bullet Outcome: "+str(outcome_bull))
+                print("==========================")
+                print("COMPROMISING")
+                poss = compromising_vote(pref_matrix[:,i].copy())
+                happy_com = []
+                outcome_com = []
+
+                for j in range(len(poss[0])):
+                        com_pref_matrix = pref_matrix.copy()
+                        com_pref_matrix[:, i] = poss[:,j]
+                        outcome_com, _ = true_voting(com_pref_matrix, voting_scheme)
+                        happy_com = happy_com + [happiness_level(i, com_pref_matrix, outcome_com)]
+
+                best_i = happy_com.index(max(happy_com))
+                print("Comp Happiness: "+str(max(happy_com)))
+                print(pref_matrix[:,i].copy())
+                print("Comp Voting: "+str(poss[:,best_i]))
+                print("Comp Outcome: "+str(outcome_com))
+                print("==========================")
+
+                tuple = ()
+                pref = []
+                output = []
+                happiness = 0
+                #If bullet is better than original
+                if(happy_orig<happy_bull):
+                        #Compromising better than bullet?
+                        if(happy_bull < max(happy_com)):
+                                pref = poss[:,best_i]
+                                output = outcome_com
+                                for j in range(0,n):
+                                        happiness += happiness_level(j,com_pref_matrix,outcome_com)
+                                #tuple = (poss[:,happy_com.index(max(happy_com))],outcome_com,happiness_level(i,com_pref_matrix,outcome_com))
+                        else:
+                                pref = pref_bull
+                                output = outcome_bull
+                                for j in range(0, n):
+                                        happiness += happiness_level(j,bull_pref_matrix,outcome_bull)
+                else:
+                        pref = pref_matrix[:,i].copy()
+                        output = ranked_outcome
+                        for j in range(0, n):
+                                happiness += happiness_level(j,pref_matrix,ranked_outcome)
+                tuple = (str(pref), str(output), happiness, happiness_level(i,pref_matrix,output)-happy_orig)
+                print("Output: (preferences, output, happiness, happiness-gain)")
+                print("preferences, output: highest number preferred")
+                print(tuple)
+                print("RESULT BEFORE: " + str(ranked_outcome))
+
 
 
 #Ranks a given vector based on the values, ranked from 1->n with 1 being the worst rank
@@ -150,6 +200,21 @@ def bullet_vote(pref_vector):
 def get_alternatives(pref_vector):
         return list(itertools.permutations(pref_vector))
 
+def compromising_vote(pref_vector):
+        print(max(pref_vector))
+        best = np.argmax(pref_vector)
+        perm = get_alternatives(pref_vector[:])
+
+        leng = len(perm)
+        res = []
+        for i in range(leng):
+                if(perm[i][best] == pref_vector[best]):
+                        res = res + [i]
+        perm = [perm[i] for i in res]
+        perm = np.array(perm)
+
+        perm = np.reshape(perm, (len(res),m)).T
+        return perm
 
 
 #Printing stuff for clarity
@@ -159,7 +224,7 @@ print("Preferences")
 print(preferences)
 print("=================")
 print("Voting scheme")
-print(borda_voting)
+print(voting_for_two)
 #print(happiness_level(i, new_pref_matrix, outcome))
-voting_analysis(preferences,borda_voting)
+voting_analysis(preferences,voting_for_two)
 
